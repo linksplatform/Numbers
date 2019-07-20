75fc3a92cbf47d444e10d0b5f74a803151bfa1c2
Константин Дьяченко <konard@me.com>
2019-07-20 14:22:08 +0300

    Initial code commit, forked from: https://github.com/Konard/LinksPlatform

---
+++ MathExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Platform.Numbers
+{
+    public static class MathExtensions
+    {
+        public static T Abs<T>(this ref T x) where T : struct => x = MathHelpers<T>.Abs(x);
+        public static T Negate<T>(this ref T x) where T : struct => x = MathHelpers<T>.Negate(x);
+    }
+}
