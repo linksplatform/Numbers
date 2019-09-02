7b7dc7a7032261bec415bc339a82f0bca12c1e3b
Константин Дьяченко <konard@me.com>
2019-09-02 15:42:10 +0300

    Codacy and CodeFactor issues fix.

--- publish-docs.sh
+++ publish-docs.sh
@@ -20,12 +20,12 @@ sed -i "s/\$TRAVIS_REPO_NAME/${TRAVIS_REPO_NAME}/g" docfx.json
 
 # DocFX installation
 nuget install docfx.console
-mono $(ls | grep "docfx.console.")/tools/docfx.exe docfx.json
+mono $(echo ./*docfx.console.*)/tools/docfx.exe docfx.json
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone https://$REPOSITORY out
-cd out
+cd out || exit
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
@@ -35,7 +35,7 @@ rm -rf out/**/* || exit 0
 # Copy genereted docs site
 cp -r _site/* out
 
-cd out
+cd out || exit
 
 # Do not use index.md
 cp README.html index.html
