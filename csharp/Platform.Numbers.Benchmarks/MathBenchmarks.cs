d0b61ec99c712658a1f7dcda78d005827b61c443
Konstantin Dyachenko <drakonard@gmail.com>
2020-07-12 19:39:20 +0300

    Factorial and Catalan methods updated to be the most efficient possible. MathBenchmarks refactoring.

--- csharp/Platform.Numbers.Benchmarks/MathBenchmarks.cs
+++ csharp/Platform.Numbers.Benchmarks/MathBenchmarks.cs
@@ -6,54 +6,170 @@ namespace Platform.Numbers.Benchmarks
     [MemoryDiagnoser]
     public class MathBenchmarks
     {
-        private const int FACTORIALTESTNUMBER = 19;
+        private class Alternatives
+        {
+            /// <remarks>
+            /// <para>Source: https://oeis.org/A000142/list </para>
+            /// <para>Источник: https://oeis.org/A000142/list </para>
+            /// </remarks>
+            private static readonly ulong[] _factorials =
+            {
+                1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
+                479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
+                355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000,
+            };
+
+            public static ulong FactorialUsingStaticArrayAndRecursionWithContantAsMaximumN(ulong n)
+            {
+                if (n <= 1)
+                {
+                    return 1;
+                }
+                if (n < 21)
+                {
+                    return _factorials[n];
+                }
+                return n * FactorialUsingStaticArrayAndRecursionWithContantAsMaximumN(n - 1);
+            }
+
+            public static ulong FactorialUsingStaticArrayAndRecursionWithArrayLengthFieldAsMaximumN(ulong n)
+            {
+                if (n <= 1)
+                {
+                    return 1;
+                }
+                if (n < (ulong)_factorials.Length)
+                {
+                    return _factorials[n];
+                }
+                return n * FactorialUsingStaticArrayAndRecursionWithArrayLengthFieldAsMaximumN(n - 1);
+            }
+
+            public static ulong FactorialWithDirectArrayAccess(ulong n)
+            {
+                return _factorials[n];
+            }
+
+            public static ulong FactorialOf19()
+            {
+                return 121645100408832000;
+            }
+
+            public static ulong ConditionSeries(ulong n)
+            {
+                if (n < 0)
+                    return 0;
+                if (n == 0)
+                    return 1;
+                if (n == 1 || n == 2)
+                    return 1;
+                if (n == 3) return 2;
+                if (n == 4) return 6;
+                if (n == 5) return 24;
+                if (n == 6) return 120;
+                if (n == 7) return 720;
+                if (n == 8) return 5040;
+                if (n == 9) return 40320;
+                if (n == 10) return 362880;
+                if (n == 11) return 3628800;
+                if (n == 12) return 39916800;
+                if (n == 13) return 479001600;
+                if (n == 14) return 6227020800;
+                if (n == 15) return 87178291200;
+                if (n == 16) return 1307674368000;
+                if (n == 17) return 20922789888000;
+                if (n == 18) return 355687428096000;
+                if (n == 19) return 6402373705728000;
+                if (n == 20) return 121645100408832000;
+                if (n == 21) return 2432902008176640000;
+                return n * ConditionSeries(n - 1);
+            }
+
+            public static ulong FactorialWhileLoopWithArray(ulong n)
+            {
+                ulong[] _facts =
+                {
+                    1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
+                    479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
+                    355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000,
+                };
+                if (n < (ulong)_facts.Length) return _facts[n];
+                ulong r = n;
+                while (n > 1) r *= --n;
+                return r;
+            }
+
+            public static ulong FactorialWhileLoopWithoutArray(ulong n)
+            {
+                if (n < (ulong)_factorials.Length) return _factorials[n];
+                ulong r = n;
+                while (n > 1) r *= --n;
+                return r;
+            }
+
+            public static ulong FactorialWhileLoopWithoutArrayAndCountingArrayLength(ulong n)
+            {
+                if (n < 21) return _factorials[n];
+                ulong r = n;
+                while (n > 1) r *= --n;
+                return r;
+            }
+        }
+
+        private const ulong FactorialNumber = 19;
 
         [Benchmark]
-        public long FactorialUsingRecursion()
+        public ulong CurrentFactorialImplementation()
         {
-            return Math.Factorial(FACTORIALTESTNUMBER);
+            return Math.Factorial(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialRecursionCountingArraylength()
+        public ulong FactorialUsingStaticArrayAndRecursionWithContantAsMaximumN()
         {
-            return Math.FactorialRecursionCountingArraylength(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialUsingStaticArrayAndRecursionWithContantAsMaximumN(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialUsingFactTree()
+        public ulong FactorialUsingStaticArrayAndRecursionWithArrayLengthFieldAsMaximumN()
         {
-            return Math.FactTree(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialUsingStaticArrayAndRecursionWithArrayLengthFieldAsMaximumN(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialOnly21()
+        public ulong FactorialUsingFactTree()
         {
-            return Math.FactorialOnly21(FACTORIALTESTNUMBER);
+            return Alternatives.ConditionSeries(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialOf19()
+        public ulong FactorialOnly21()
         {
-            return Math.FactorialOf19(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialWithDirectArrayAccess(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialWhileWithArray()
+        public ulong FactorialOf19()
         {
-            return Math.FactorialWhileWithArray(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialOf19();
         }
 
         [Benchmark]
-        public long FactorialWhileWithoutArray()
+        public ulong FactorialWhileWithArray()
         {
-            return Math.FactorialWhileWithoutArray(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialWhileLoopWithArray(FactorialNumber);
         }
 
         [Benchmark]
-        public long FactorialWhileWithoutArrayAndCountingArrayLength()
+        public ulong FactorialWhileWithoutArray()
         {
-            return Math.FactorialWhileWithoutArrayAndCountingArrayLength(FACTORIALTESTNUMBER);
+            return Alternatives.FactorialWhileLoopWithoutArray(FactorialNumber);
+        }
+
+        [Benchmark]
+        public ulong FactorialWhileWithoutArrayAndCountingArrayLength()
+        {
+            return Alternatives.FactorialWhileLoopWithoutArrayAndCountingArrayLength(FactorialNumber);
         }
     }
 }
--- csharp/Platform.Numbers/Math.cs
+++ csharp/Platform.Numbers/Math.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
@@ -16,193 +14,64 @@ namespace Platform.Numbers
         /// <para>Source: https://oeis.org/A000142/list </para>
         /// <para>Источник: https://oeis.org/A000142/list </para>
         /// </remarks>
-        private static readonly long[] _factorials =
+        private static readonly ulong[] _factorials =
         {
             1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
             479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
-            355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000,
+            355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000
         };
 
         /// <remarks>
         /// <para>Source: https://oeis.org/A000108/list </para>
         /// <para>Источник: https://oeis.org/A000108/list </para>
         /// </remarks>
-        private static readonly long[] _catalans =
+        private static readonly ulong[] _catalans =
         {
-            1,  1,  2,  5,  14,  42,  132,  429,  1430,  4862,  16796,  58786,  208012, 
-            742900,  2674440,  9694845,  35357670,  129644790,  477638700,  1767263190, 
-            6564120420,  24466267020,  91482563640,  343059613650,  1289904147324,  4861946401452, 
-            18367353072152,  69533550916004,  263747951750360,  1002242216651368,  3814986502092304
+            1,  1,  2,  5,  14,  42,  132,  429,  1430,  4862,  16796,  58786,  208012,
+            742900,  2674440,  9694845,  35357670,  129644790,  477638700,  1767263190,
+            6564120420,  24466267020,  91482563640,  343059613650,  1289904147324,  4861946401452,
+            18367353072152,  69533550916004,  263747951750360,  1002242216651368,  3814986502092304,
+            14544636039226909, 55534064877048198, 212336130412243110, 812944042149730764, 3116285494907301262, 11959798385860453492
         };
 
+        public static readonly ulong MaximumFactorialNumber = 20;
+
+        public static readonly ulong MaximumCatalanIndex = 36;
+
         /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
+        /// <para>Returns the product of all positive integers less than or equal to the number specified as an argument.</para>
+        /// <para>Возвращает произведение всех положительных чисел меньше или равных указанному в качестве аргумента числу.</para>
         /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long Factorial(long n)
+        /// <param name="n"><para>The maximum positive number that will participate in factorial's product.</para><para>Максимальное положительное число, которое будет участвовать в произведение факториала.</para></param>
+        /// <returns><para>The product of all positive integers less than or equal to the number specified as an argument.</para><para>Произведение всех положительных чисел меньше или равных указанному в качестве аргумента числу.</para></returns>
+        public static ulong Factorial(ulong n)
         {
-            if (n <= 1)
-            {
-                return 1;
-            }
-          
-          
-            if (n < 21)
+            if (n >= 0 && n <= MaximumFactorialNumber)
             {
                 return _factorials[n];
             }
-            
-            return n * Factorial(n - 1);
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactorialRecursionCountingArraylength(long n)
-        {
-            if (n <= 1)
+            else
             {
-                return 1;
+                throw new ArgumentOutOfRangeException($"Only numbers from 0 to {MaximumFactorialNumber} are supported by unsigned integer with 64 bits length.");
             }
+        }
 
-
-            if (n < _factorials.Length)
+        /// <summary>
+        /// <para>Returns the Catalan Number with the number specified as an argument.</para>
+        /// <para>Возвращает Каталановое число с номером указанным в качестве аргумента.</para>
+        /// </summary>
+        /// <param name="n"><para>The number of Catalan number.</para><para>Номер Каталанового числа.</para></param>
+        /// <returns><para>The Catalan Number with the number specified as an argument.</para><para>Каталановое число с номером указанным в качестве аргумента.</para></returns>
+        public static ulong Catalan(ulong n)
+        {
+            if (n >= 0 && n <= MaximumCatalanIndex)
             {
-                return _factorials[n];
+                return _catalans[n];
             }
-
-            return n * Factorial(n - 1);
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactorialOnly21(long n)
-        {
-            return _factorials[n];
-        }
-
-        public static long FactorialOf19(long n)
-        {
-            return 121645100408832000;
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactTree(int n)
-        {
-            if (n < 0)
-                return 0;
-            if (n == 0)
-                return 1;
-            if (n == 1 || n == 2)
-                return 1;
-            if (n == 3) return 2;
-            if (n == 4) return 6;
-            if (n == 5) return 24;
-            if (n == 6) return 120;
-            if (n == 7) return 720;
-            if (n == 8) return 5040;
-            if (n == 9) return 40320;
-            if (n == 10) return 362880;
-            if (n == 11) return 3628800;
-            if (n == 12) return 39916800;
-            if (n == 13) return 479001600;
-            if (n == 14) return 6227020800;
-            if (n == 15) return 87178291200;
-            if (n == 16) return 1307674368000;
-            if (n == 17) return 20922789888000;
-            if (n == 18) return 355687428096000;
-            if (n == 19) return 6402373705728000;
-            if (n == 20) return 121645100408832000;
-            if (n == 21) return 2432902008176640000;
-
-            return n * Factorial(n - 1);
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactorialWhileWithArray(long n)
-        {
-            long[] _facts =
+            else
             {
-                1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
-                479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
-                355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000,
-            };
-            if (n < _facts.Length) return _facts[n];
-
-            long r = n;
-            while (n > 1) r *= --n;
-            return r;
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactorialWhileWithoutArray(long n)
-        {
-            if (n < _factorials.Length) return _factorials[n];
-
-            long r = n;
-            while (n > 1) r *= --n;
-            return r;
-        }
-
-        /// <summary>
-        /// <para>Generate the factorial of the value "n".</para>
-        /// <para>Генерация факториaла из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Factorial generation value.</para><para>Значение генерации факториала.</para></param>
-        /// <returns><para>Result of factorial calculation.</para><para>Результат подсчета факториала</para></returns>
-        public static long FactorialWhileWithoutArrayAndCountingArrayLength(long n)
-        {
-            if (n < 21) return _factorials[n];
-
-            long r = n;
-            while (n > 1) r *= --n;
-            return r;
-        }
-
-
-        /// <summary>
-        /// <para>Generating the Catalan Number of the value "n".</para>
-        /// <para>Генерация числа Каталана из значения переменной "n".</para>
-        /// </summary>
-        /// <param name="n"><para>Catalan Number generation value.</para><para>Значение генерации Числа Каталана.</para></param>
-        /// <returns><para>Result of Catalan Number calculation.</para><para>Результат подсчета Числа Каталана.</para></returns>
-        public static long Catalan(int n)
-        {
-           
-           
-            if (n <= 1)
-            {
-                return 1;
+                throw new ArgumentOutOfRangeException($"Only numbers from 0 to {MaximumCatalanIndex} are supported by unsigned integer with 64 bits length.");
             }
-            if (n < _catalans.Length)
-            {
-                return _catalans[(int)n];
-            }
-            return Factorial(2 * n) / (Factorial(n + 1) * Factorial(n));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
