7580229b10927f0796fbb969967e587f3bbf4c94
Mask <rebootroot13@gmail.com>
2020-07-25 13:59:13 +0300

    Added <para> ... </para>

--- csharp/Platform.Numbers/Arithmetic.cs
+++ csharp/Platform.Numbers/Arithmetic.cs
@@ -1,101 +1,101 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Numbers
 {
     /// <summary>
-    /// Each function performs the simplest arithmetic operation on the type specified as a parameter
-    /// Каждая функция выполняет простейшую арифметическую операцию над типом, указанным в качестве параметра.
+    /// <para> Each function performs the simplest arithmetic operation on the type specified as a parameter </para>
+    /// <para> Каждая функция выполняет простейшую арифметическую операцию над типом, указанным в качестве параметра </para>
     /// </summary>
     public static class Arithmetic
     {
 
         /// <summary>
-        /// Adding the x and y arguments
-        /// Сложение аргументов х и у
+        /// <para> Adding the x and y arguments </para>
+        /// <para> Сложение аргументов х и у </para>
         /// </summary>
         /// <returns>
-        /// Sum of x and y
-        /// Сумма x и y
+        /// <para> Sum of x and y </para>
+        /// <para> Сумма x и y </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Add<T>(T x, T y) => Arithmetic<T>.Add(x, y);
 
         /// <summary>
-        /// Subtracting x from y
-        /// Вычитание х из у
+        /// <para> Subtracting x from y </para>
+        /// <para> Вычитание х из у </para>
         /// </summary>
         /// <returns>
-        /// Difference of x and y
-        /// Разность x и y
+        /// <para> Difference of x and y </para>
+        /// <para> Разность x и y </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Subtract<T>(T x, T y) => Arithmetic<T>.Subtract(x, y);
 
         /// <summary>
-        /// Multiplication the x and y
-        /// Умножение х и у
+        /// <para> Multiplication the x and y </para>
+        /// <para> Умножение х и у </para>
         /// </summary>
         /// <returns>
         /// <returns>
-        /// Product of x and y
-        /// Произведение x и y
+        /// <para> Product of x and y </para>
+        /// <para> Произведение x и y </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Multiply<T>(T x, T y) => Arithmetic<T>.Multiply(x, y);
 
         /// <summary>
-        /// Dividing x by y
-        /// Деление х на у
+        /// <para> Dividing x by y </para>
+        /// <para> Деление х на у </para>
         /// </summary>
         /// <returns>
-        /// Quoitent of x and y
-        /// Частное x и y
+        /// <para> Quoitent of x and y </para>
+        /// <para> Частное x и y </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Divide<T>(T x, T y) => Arithmetic<T>.Divide(x, y);
 
         /// <summary>
-        /// Increasing the parameter x by one
-        /// Увеличение параметра x на единицу
+        /// <para> Increasing the parameter x by one </para>
+        /// <para> Увеличение параметра x на единицу </para>
         /// </summary>
         /// <returns>
-        /// Increase by one x
-        /// Увеличенное на единицу x
+        /// <para> Increase by one x </para>
+        /// <para> Увеличенное на единицу x </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(T x) => Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// Increase the parameter x passed by reference by one
-        /// Увеличение переданного по ссылке параметра x на единицу
+        /// <para> Increase the parameter x passed by reference by one </para>
+        /// <para> Увеличение переданного по ссылке параметра x на единицу </para>
         /// </summary>
         /// <returns>
-        /// Increase by one x with returning the value to the original variable
-        /// Увеличенное на единицу x с возвратом значения в исходную переменную
+        /// <para> Increase by one x with returning the value to the original variable </para>
+        /// <para> Увеличенное на единицу x с возвратом значения в исходную переменную </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(ref T x) => x = Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// Decrease parameter x by one
-        /// Уменьшение параметра x на единицу
+        /// <para> Decrease parameter x by one </para>
+        /// <para> Уменьшение параметра x на единицу </para>
         /// </summary>
         /// <returns>
-        /// Reduced by one x
-        /// Уменьшенное на единицу x
+        /// <para> Reduced by one x </para>
+        /// <para> Уменьшенное на единицу x </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(T x) => Arithmetic<T>.Decrement(x);
 
         /// <summary>
-        /// Decreasing the parameter x passed by reference by one
-        /// Уменьшение переданного по ссылке параметра x на единицу
+        /// <para> Decreasing the parameter x passed by reference by one </para>
+        /// <para> Уменьшение переданного по ссылке параметра x на единицу </para>
         /// </summary>
         /// <returns>
-        /// Reduced by one x with returning the value to the original variable
-        /// Уменьшенное на единицу x с воз вратом значения в исходную переменную
+        /// <para> Reduced by one x with returning the value to the original variable </para>
+        /// <para> Уменьшенное на единицу x с воз вратом значения в исходную переменную </para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(ref T x) => x = Arithmetic<T>.Decrement(x);
