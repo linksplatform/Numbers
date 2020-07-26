5f7644b532e506bc979d47c186f283946c5a7599
Mask <rebootroot13@gmail.com>
2020-07-27 00:37:10 +0300

    Added xml comments

--- csharp/Platform.Numbers/MathExtensions.cs
+++ csharp/Platform.Numbers/MathExtensions.cs
@@ -6,9 +6,25 @@ namespace Platform.Numbers
 {
     public static class MathExtensions
     {
+        /// <summary>
+        /// <para> Takes a module from a number </para>
+        /// <para> Берёт модуль от числа </para>
+        /// </summary>
+        /// <returns>
+        /// <para> The absolute value of a number </para>
+        /// <para> Абсолютное значение числа </para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Abs<T>(this ref T x) where T : struct => x = Math<T>.Abs(x);
 
+        /// <summary>
+        /// <para> Makes a number negatory </para>
+        /// <para> Делает число отрицательным </para>
+        /// </summary>
+        /// <returns>
+        /// <para> Negatory number </para>
+        /// <para> Отрицательное число </para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Negate<T>(this ref T x) where T : struct => x = Math<T>.Negate(x);
     }
