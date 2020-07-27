2fde607718618082aa05bcfe0be4633399ede0cc
Mask <rebootroot13@gmail.com>
2020-07-27 11:29:35 +0300

    Added <param> ... </param>

--- csharp/Platform.Numbers/MathExtensions.cs
+++ csharp/Platform.Numbers/MathExtensions.cs
@@ -5,34 +5,30 @@
 namespace Platform.Numbers
 {
     /// <summary>
-    /// <para> Additional mathematical transformations </para>
-    /// <para> Дополнительные математические преобразования </para>
+    /// <para>AProvides a set of extension methods that perform mathematical operations on arbitrary object types.</para>
+    /// <para>Предоставляет набор методов расширения выполняющих математические операции для объектов произвольного типа.</para>
     /// </summary>
-    /// <returns>
-    /// <para> The converted value in the original variable </para>
-    /// <para> Преобразованное значение в исходной переменной </para>
-    /// </returns>
     public static class MathExtensions
     {
         /// <summary>
-        /// <para> Takes a module from a number </para>
-        /// <para> Берёт модуль от числа </para>
+        /// <para>Takes a module from a number.</para>
+        /// <para>Берёт модуль от числа.</para>
         /// </summary>
-        /// <returns>
-        /// <para> The absolute value of a number </para>
-        /// <para> Абсолютное значение числа </para>
-        /// </returns>
+        /// <param name = "x">
+        /// <para>The number from which to take the absolute value.</para>
+        /// <para>Число от которого необходимо взять абсолютное значение.</para>
+        /// </param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Abs<T>(this ref T x) where T : struct => x = Math<T>.Abs(x);
 
         /// <summary>
-        /// <para> Makes a number negatory </para>
-        /// <para> Делает число отрицательным </para>
+        /// <para>Makes a number negative.</para>
+        /// <para>Делает число отрицательным.</para>
         /// </summary>
-        /// <returns>
-        /// <para> Negatory number </para>
-        /// <para> Отрицательное число </para>
-        /// </returns>
+        /// <param>
+        /// <para>The number to be made nrgative.</para>
+        /// <para>Число которое нужно сделать отрицательным.</para>
+        /// </param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Negate<T>(this ref T x) where T : struct => x = Math<T>.Negate(x);
     }
