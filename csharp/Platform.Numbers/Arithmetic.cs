07cafe9756bffe973c84f2d5a90d0e0af6d08fd3
Mask <rebootroot13@gmail.com>
2020-07-27 11:30:29 +0300

    Fixed design decisions

--- csharp/Platform.Numbers/Arithmetic.cs
+++ csharp/Platform.Numbers/Arithmetic.cs
@@ -5,97 +5,97 @@ using System.Runtime.CompilerServices;
 namespace Platform.Numbers
 {
     /// <summary>
-    /// <para> Each function performs the simplest arithmetic operation on the type specified as a parameter </para>
-    /// <para> Каждая функция выполняет простейшую арифметическую операцию над типом, указанным в качестве параметра </para>
+    /// <para>Each function performs the simplest arithmetic operation on the type specified as a parameter.</para>
+    /// <para>Каждая функция выполняет простейшую арифметическую операцию над типом, указанным в качестве параметра.</para>
     /// </summary>
     public static class Arithmetic
     {
 
         /// <summary>
-        /// <para> Adding the x and y arguments </para>
-        /// <para> Сложение аргументов х и у </para>
+        /// <para>Adding the x and y arguments.</para>
+        /// <para>Сложение аргументов х и у.</para>
         /// </summary>
         /// <returns>
-        /// <para> Sum of x and y </para>
-        /// <para> Сумма x и y </para>
+        /// <para>Sum of x and y.</para>
+        /// <para>Сумма x и y.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Add<T>(T x, T y) => Arithmetic<T>.Add(x, y);
 
         /// <summary>
-        /// <para> Subtracting x from y </para>
-        /// <para> Вычитание х из у </para>
+        /// <para>Subtracting x from y.</para>
+        /// <para>Вычитание х из у.</para>
         /// </summary>
         /// <returns>
-        /// <para> Difference of x and y </para>
-        /// <para> Разность x и y </para>
+        /// <para>Difference of x and y.</para>
+        /// <para>Разность x и y.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Subtract<T>(T x, T y) => Arithmetic<T>.Subtract(x, y);
 
         /// <summary>
-        /// <para> Multiplication the x and y </para>
-        /// <para> Умножение х и у </para>
+        /// <para>Multiplication the x and y.</para>
+        /// <para>Умножение х и у.</para>
         /// </summary>
         /// <returns>
         /// <returns>
-        /// <para> Product of x and y </para>
-        /// <para> Произведение x и y </para>
+        /// <para>Product of x and y.</para>
+        /// <para>Произведение x и y.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Multiply<T>(T x, T y) => Arithmetic<T>.Multiply(x, y);
 
         /// <summary>
-        /// <para> Dividing x by y </para>
-        /// <para> Деление х на у </para>
+        /// <para>Dividing x by y.</para>
+        /// <para>Деление х на у.</para>
         /// </summary>
         /// <returns>
-        /// <para> Quoitent of x and y </para>
-        /// <para> Частное x и y </para>
+        /// <para>Quoitent of x and y.</para>
+        /// <para>Частное x и y.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Divide<T>(T x, T y) => Arithmetic<T>.Divide(x, y);
 
         /// <summary>
-        /// <para> Increasing the parameter x by one </para>
-        /// <para> Увеличение параметра x на единицу </para>
+        /// <para>Increasing the parameter x by one.</para>
+        /// <para>Увеличение параметра x на единицу.</para>
         /// </summary>
         /// <returns>
-        /// <para> Increase by one x </para>
-        /// <para> Увеличенное на единицу x </para>
+        /// <para>Increase by one x.</para>
+        /// <para>Увеличенное на единицу x.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(T x) => Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// <para> Increase the parameter x passed by reference by one </para>
-        /// <para> Увеличение переданного по ссылке параметра x на единицу </para>
+        /// <para>Increase the parameter x passed by reference by one.</para>
+        /// <para>Увеличение переданного по ссылке параметра x на единицу.</para>
         /// </summary>
         /// <returns>
-        /// <para> Increase by one x with returning the value to the original variable </para>
-        /// <para> Увеличенное на единицу x с возвратом значения в исходную переменную </para>
+        /// <para>Increase by one x with returning the value to the original variable.</para>
+        /// <para>Увеличенное на единицу x с возвратом значения в исходную переменную.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(ref T x) => x = Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// <para> Decrease parameter x by one </para>
-        /// <para> Уменьшение параметра x на единицу </para>
+        /// <para>Decrease parameter x by one.</para>
+        /// <para>Уменьшение параметра x на единицу.</para>
         /// </summary>
         /// <returns>
-        /// <para> Reduced by one x </para>
-        /// <para> Уменьшенное на единицу x </para>
+        /// <para>Reduced by one x.</para>
+        /// <para>Уменьшенное на единицу x.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(T x) => Arithmetic<T>.Decrement(x);
 
         /// <summary>
-        /// <para> Decreasing the parameter x passed by reference by one </para>
-        /// <para> Уменьшение переданного по ссылке параметра x на единицу </para>
+        /// <para>Decreasing the parameter x passed by reference by one.</para>
+        /// <para>Уменьшение переданного по ссылке параметра x на единицу.</para>
         /// </summary>
         /// <returns>
-        /// <para> Reduced by one x with returning the value to the original variable </para>
-        /// <para> Уменьшенное на единицу x с воз вратом значения в исходную переменную </para>
+        /// <para>Reduced by one x with returning the value to the original variable.</para>
+        /// <para>Уменьшенное на единицу x с воз вратом значения в исходную переменную.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(ref T x) => x = Arithmetic<T>.Decrement(x);
--- csharp/Platform.Numbers/Bit.cs
+++ csharp/Platform.Numbers/Bit.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+<para>﻿using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -7,22 +7,19 @@
 namespace Platform.Numbers
 {
     /// <summary>
-    /// <para> Various operations on the set number bits </para>
-    /// <para> Различные операции над установленными ьитами числа </para>
+    /// <para>Various operations on the set number bits.</para>
+    /// <para>Различные операции над установленными битами числа.</para>
     /// </summary>
-    /// <returns>
-    /// <para> Data on the established beta numbers </para>
-    /// <para> Данные об установленных бетах числа </para>
 
     public static class Bit
     {
         /// <summary>
-        /// <para> Counts the number of bits set in a number </para>
-        /// <para> Подсчитывает количество установленных бит в числе </para>
+        /// <para>Counts the number of bits set in a number.</para>
+        /// <para>Подсчитывает количество установленных бит в числе.</para>
         /// </summary>
         /// <returns>
-        /// <para> Number of bits set in a number </para>
-        /// <para> Количество установленных бит в числе </para>
+        /// <para>Number of bits set in a number.</para>
+        /// <para>Количество установленных бит в числе.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static long Count(long x)
@@ -36,12 +33,12 @@ namespace Platform.Numbers
             return n;
         }
         /// <summary>
-        /// <para> Searches for the first bit set in a number </para>
-        /// <para> Ищет первый установленный бит в числе </para>
+        /// <para>Searches for the first bit set in a number.</para>
+        /// <para>Ищет первый установленный бит в числе.</para>
         /// </summary>
         /// <returns>
-        /// <para> First bit set </para>
-        /// <para> Первый установленный бит </para>
+        /// <para>First bit set.</para>
+        /// <para>Первый установленный бит.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetLowestPosition(ulong value)
@@ -59,80 +56,80 @@ namespace Platform.Numbers
             return position;
         }
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Not<T>(T x) => Bit<T>.Not(x);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Or<T>(T x, T y) => Bit<T>.Or(x, y);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T And<T>(T x, T y) => Bit<T>.And(x, y);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ShiftLeft<T>(T x, int y) => Bit<T>.ShiftLeft(x, y);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ShiftRight<T>(T x, int y) => Bit<T>.ShiftRight(x, y);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T PartialWrite<T>(T target, T source, int shift, int limit) => Bit<T>.PartialWrite(target, source, shift, limit);
 
-        ///<summary>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</summary>
-        ///<returns>
-        ///<para>  </para>
-        ///<para>  </para>
-        ///</returns>  
+        /// <summary>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </summary>
+        /// <returns>
+        /// <para>.</para>
+        /// <para>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T PartialRead<T>(T target, int shift, int limit) => Bit<T>.PartialRead(target, shift, limit);
     }
