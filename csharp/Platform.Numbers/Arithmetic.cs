fe0ff305de3bb086002af871e3dcc507c6c0e54a
Mask <rebootroot13@gmail.com>
2020-07-28 03:04:49 +0300

    Changed translation

--- csharp/Platform.Numbers/Arithmetic.cs
+++ csharp/Platform.Numbers/Arithmetic.cs
@@ -10,16 +10,15 @@ namespace Platform.Numbers
     /// </summary>
     public static class Arithmetic
     {
-
         /// <summary>
-        /// <para>Adding the x and y arguments.</para>
-        /// <para>Сложение аргументов х и у.</para>
+        /// <para>Performing adding the x and y arguments.</para>
+        /// <para>Выполняется ложение аргументов х и у.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>The first term.</para>
         /// <para>Первое слагаемое.</para>
         /// </param>
-        /// <param name = "y">
+        /// <param name="y">
         /// <para>The second term.</para>
         /// <para>Второе слагаемое.</para>
         /// </param>
@@ -31,14 +30,14 @@ namespace Platform.Numbers
         public static T Add<T>(T x, T y) => Arithmetic<T>.Add(x, y);
 
         /// <summary>
-        /// <para>Subtracting x from y.</para>
-        /// <para>Вычитание х из у.</para>
+        /// <para>Permorming subtracting x from y.</para>
+        /// <para>Выполняется вычитание х из у.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>Minuend.</para>
         /// <para>Уменьшаемое.</para>
         /// </param>
-        /// <param name = "y">
+        /// <param name="y">
         /// <para>Subtrahend.</para>
         /// <para>Вычитаемое.</para>
         /// </param>
@@ -50,14 +49,14 @@ namespace Platform.Numbers
         public static T Subtract<T>(T x, T y) => Arithmetic<T>.Subtract(x, y);
 
         /// <summary>
-        /// <para>Multiplication the x and y.</para>
-        /// <para>Умножение х и у.</para>
+        /// <para>Permorming multiplication the x and y.</para>
+        /// <para>Выполняется умножение х и у.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>First multiplier.</para>
         /// <para>Первый множитель.</para>
         /// </param>
-        /// <param name = "y">
+        /// <param name="y">
         /// <para>Second multiplier.</para>
         /// <para>Второй множитель.</para>
         /// </param>
@@ -69,14 +68,14 @@ namespace Platform.Numbers
         public static T Multiply<T>(T x, T y) => Arithmetic<T>.Multiply(x, y);
 
         /// <summary>
-        /// <para>Dividing x by y.</para>
-        /// <para>Деление х на у.</para>
+        /// <para>Permorming dividing x by y.</para>
+        /// <para>Выполняется деление х на у.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>Dividend.</para>
         /// <para>Делимое.</para>
         /// </param>
-        /// <param name = "y">
+        /// <param name="y">
         /// <para>Divider.</para>
         /// <para>Делитель.</para>
         /// </param>
@@ -91,58 +90,58 @@ namespace Platform.Numbers
         /// <para>Increasing the parameter x by one.</para>
         /// <para>Увеличение параметра x на единицу.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>Increase the number required.</para>
         /// <para>Число необходимое увеличить.</para>
         /// </param>
         /// <returns>
-        /// <para>Increase by one x.</para>
-        /// <para>Увеличенное на единицу x.</para>
+        /// <para>Increase by one number x.</para>
+        /// <para>Увеличенное на единицу число x.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(T x) => Arithmetic<T>.Increment(x);
 
         /// <summary>
         /// <para>Increase the parameter x passed by reference by one.</para>
-        /// <para>Увеличение переданного по ссылке параметра x на единицу.</para>
+        /// <para>Увеличение значения аргумента x на единицу.</para>
         /// </summary>
-        /// <param name = "x">
+        /// <param name="x">
         /// <para>Increase the argument required.</para>
         /// <para>Аргумент требуемый увеличить.</para>
         /// </param>
         /// <returns>
-        /// <para>Increase by one x with returning the value to the original variable.</para>
-        /// <para>Увеличенное на единицу x с возвратом значения в исходную переменную.</para>
+        /// <para>Increased argument x value.</para>
+        /// <para>Увеличенное значение аргумента x</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(ref T x) => x = Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// <para>Decrease parameter x by one.</para>
-        /// <para>Уменьшение параметра x на единицу.</para>
+        /// <para>Decrease number x by one.</para>
+        /// <para>Уменьшение числа x на единицу.</para>
         /// </summary>
-        /// <param name = "x">
-        /// <para>Increase the number reduce.</para>
+        /// <param name="x">
+        /// <para>Require the number reduce.</para>
         /// <para>Число необходимое уменьшить.</para>
         /// </param>
         /// <returns>
-        /// <para>Reduced by one x.</para>
-        /// <para>Уменьшенное на единицу x.</para>
+        /// <para>Decreased by one number x.</para>
+        /// <para>Уменьшенное на единицу число x.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(T x) => Arithmetic<T>.Decrement(x);
 
         /// <summary>
-        /// <para>Decreasing the parameter x passed by reference by one.</para>
-        /// <para>Уменьшение переданного по ссылке параметра x на единицу.</para>
+        /// <para>Decreases the value of the argument x by one.</para>
+        /// <para>Уменьшение значения аргумента x на единицу.</para>
         /// </summary>
-        /// <param name = "x">
-        /// <para>Increase the argument reduce.</para>
+        /// <param name="x">
+        /// <para>Require the argument reduce.</para>
         /// <para>Аргумент требуемый уменьшить.</para>
         /// </param>
         /// <returns>
-        /// <para>Reduced by one x with returning the value to the original variable.</para>
-        /// <para>Уменьшенное на единицу x с воз вратом значения в исходную переменную.</para>
+        /// <para>Decreased argument x value.</para>
+        /// <para>Уменьшеное значение аргумента x.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(ref T x) => x = Arithmetic<T>.Decrement(x);
