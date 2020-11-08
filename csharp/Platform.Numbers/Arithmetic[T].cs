c89c013515d97cb80b6828be1b6149fda21d917b
Mask <rebootroot13@gmail.com>
2020-11-08 18:09:00 +0300

    Arithmetic[T].cs fixed

--- csharp/Platform.Numbers/Arithmetic[T].cs
+++ csharp/Platform.Numbers/Arithmetic[T].cs
@@ -51,7 +51,7 @@ namespace Platform.Numbers
         private static Func<T, T, T> CompileAddDelegate()
         {
            return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
-           {
+            {
                Ensure.Always.IsNumeric<T>();
                emiter.LoadArguments(0, 1);
                emiter.Add();
@@ -62,8 +62,8 @@ namespace Platform.Numbers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, T, T> CompileSubtractDelegate()
         {
-           return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
-           {
+            return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
+            {
                Ensure.Always.IsNumeric<T>();
                emiter.LoadArguments(0, 1);
                emiter.Subtract();
@@ -74,32 +74,32 @@ namespace Platform.Numbers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, T, T> CompileMultiplyDelegate()
         {
-           return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
-           {
-               Ensure.Always.IsNumeric<T>();
-               emiter.LoadArguments(0, 1);
-               emiter.Emit(OpCodes.Mul);
-               emiter.Return();
-           });
+            return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
+            {
+                Ensure.Always.IsNumeric<T>();
+                emiter.LoadArguments(0, 1);
+                emiter.Emit(OpCodes.Mul);
+                emiter.Return();
+            });
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, T, T> CompileDivideDelegate()
         {
-           return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
-           {
-               Ensure.Always.IsNumeric<T>();
-               emiter.LoadArguments(0, 1);
-               if(NumericType<T>.IsSigned)
-               {
+            return DelegateHelpers.Compile<Func<T, T, T>>(emiter =>
+            {
+                Ensure.Always.IsNumeric<T>();
+                emiter.LoadArguments(0, 1);
+                if(NumericType<T>.IsSigned)
+                {
                  emiter.Emit(OpCodes.Div);
-               }
-               else
-               {
+                }
+                else
+                {
                   emiter.Emit(OpCodes.Div_Un);
-               }
-               emiter.Return();
-           });
+                }
+                emiter.Return();
+            });
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -107,22 +107,22 @@ namespace Platform.Numbers
         {
            return DelegateHelpers.Compile<Func<T, T>>(emiter =>
            {
-               Ensure.Always.IsNumeric<T>();
-               emiter.LoadArgument(0);
-               emiter.Increment<T>();
-               emiter.Return();
+                Ensure.Always.IsNumeric<T>();
+                emiter.LoadArgument(0);
+                emiter.Increment<T>();
+                emiter.Return();
            });
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, T> CompileDecrementDelegate()
         {
-           return DelegateHelpers.Compile<Func<T, T>>(emiter =>
-           {
-               Ensure.Always.IsNumeric<T>();
-               emiter.LoadArgument(0);
-               emiter.Decrement<T>();
-               emiter.Return();
+            return DelegateHelpers.Compile<Func<T, T>>(emiter =>
+            {
+                Ensure.Always.IsNumeric<T>();
+                emiter.LoadArgument(0);
+                emiter.Decrement<T>();
+                emiter.Return();
            });
         }
     }
