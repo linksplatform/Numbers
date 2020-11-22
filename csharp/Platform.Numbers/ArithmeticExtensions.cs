49eb90b89d19d9487f35f8e2903fdfd55d6271ad
mrMask <rebootroot13@gmail.com>
2020-11-23 01:48:19 +0300

    Added description in ArithmeticExtensions.cs

--- csharp/Platform.Numbers/ArithmeticExtensions.cs
+++ csharp/Platform.Numbers/ArithmeticExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -11,39 +11,39 @@ namespace Platform.Numbers
     public static class ArithmeticExtensions
     {
         /// <summary>
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>Increasing the parameter by one.</para>
+        /// <para>Увеличение параметра на единицу.</para>
         /// </summary>
         /// <typeparam name="T">
         /// <para>The number type.</para>
         /// <para>Тип числа.</para>
         /// </typeparam>
         /// <param name="x">
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>The parameter to increase.</para>
+        /// <para>Параметр для увеличения.</para>
         /// </param>
         /// <returns>
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>Increased the value of the parameter per unit.</para>
+        /// <para>Увеличенное значение параметра на единицу.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(this ref T x) where T : struct => x = Arithmetic<T>.Decrement(x);
 
         /// <summary>
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>Decreases parameter by one.</para>
+        /// <para>Уменьшение параметра на единицу.</para>
         /// </summary>
         /// <typeparam name="T">
         /// <para>The number type.</para>
         /// <para>Тип числа.</para>
         /// </typeparam>
         /// <param name="x">
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>The parameter to decrease.</para>
+        /// <para>Параметр для уменьшения.</para>
         /// </param>
         /// <returns>
-        /// <para>.</para>
-        /// <para>.</para>
+        /// <para>Decreased the value of the parameter per unit.</para>
+        /// <para>Уменьшенное на единицу значение параметра.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(this ref T x) where T : struct => x = Arithmetic<T>.Increment(x);
