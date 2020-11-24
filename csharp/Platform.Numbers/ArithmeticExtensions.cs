46f73aa4d9a258ffbe9bea0f304a8351371f5899
mrMask <rebootroot13@gmail.com>
2020-11-24 21:34:38 +0300

    Fixed lexical terms

--- csharp/Platform.Numbers/ArithmeticExtensions.cs
+++ csharp/Platform.Numbers/ArithmeticExtensions.cs
@@ -5,45 +5,45 @@ using System.Runtime.CompilerServices;
 namespace Platform.Numbers
 {
     /// <summary>
-    /// <para>Implements unary mathematical operations.</para>
-    /// <para>Реализует унарные математические операции.</para>
+    /// <para>Provides a set of extension methods that perform mathematical operations on arbitrary object types.</para>
+    /// <para>Предоставляет набор методов расширения выполняющих математические операции для объектов произвольного типа.</para>
     /// </summary>
     public static class ArithmeticExtensions
     {
         /// <summary>
-        /// <para>Increasing the parameter by one.</para>
-        /// <para>Увеличение параметра на единицу.</para>
+        /// <para>Increments the variable passed as an argument by one.</para>
+        /// <para>Увеличивает переданную в качестве аргумента переменную на единицу.</para>
         /// </summary>
         /// <typeparam name="T">
         /// <para>The number type.</para>
         /// <para>Тип числа.</para>
         /// </typeparam>
         /// <param name="x">
-        /// <para>The parameter to increase.</para>
-        /// <para>Параметр для увеличения.</para>
+        /// <para>Reference to the variable being incremented.</para>
+        /// <para>Ссылка на увеличиваемую переменную.</para>
         /// </param>
         /// <returns>
-        /// <para>Increased the value of the parameter per unit.</para>
-        /// <para>Увеличенное значение параметра на единицу.</para>
+        /// <para>Increased the value of the argument by one.</para>
+        /// <para>Увеличенное значение аргумента на единицу.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Increment<T>(this ref T x) where T : struct => x = Arithmetic<T>.Increment(x);
 
         /// <summary>
-        /// <para>Decreases parameter by one.</para>
-        /// <para>Уменьшение параметра на единицу.</para>
+        /// <para>Reduces the variable passed as an argument by one.</para>
+        /// <para>Уменьшает переданную в качестве аргумента переменную на единицу.</para>
         /// </summary>
         /// <typeparam name="T">
         /// <para>The number type.</para>
         /// <para>Тип числа.</para>
         /// </typeparam>
         /// <param name="x">
-        /// <para>The parameter to decrease.</para>
-        /// <para>Параметр для уменьшения.</para>
+        /// <para>Reference to a reduced variable.</para>
+        /// <para>Ссылка на увеличиваемую переменную.</para>
         /// </param>
         /// <returns>
-        /// <para>Decreased the value of the parameter per unit.</para>
-        /// <para>Уменьшенное на единицу значение параметра.</para>
+        /// <para>Reduces the value of the argument by one.</para>
+        /// <para>Уменьшеное значение аргумента на единицу.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Decrement<T>(this ref T x) where T : struct => x = Arithmetic<T>.Decrement(x);
