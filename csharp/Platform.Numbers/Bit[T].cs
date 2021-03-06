fea14809975040306273fb98d8e4d835a55dc88e
mrMask <rebootroot13@gmail.com>
2021-03-06 20:30:10 +0300

    Deleted #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member from all files

--- csharp/Platform.Numbers/Bit[T].cs
+++ csharp/Platform.Numbers/Bit[T].cs
@@ -4,7 +4,6 @@ using Platform.Exceptions;
 using Platform.Reflection;
 
 // ReSharper disable StaticFieldInGenericType
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Numbers
 {
