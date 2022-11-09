3e08bd35ab14a19c99cf4ed5928402f49a8acd69
Daulet <uralskdev@gmail.com>
2022-11-09 20:30:08 +0600

    Translated PartialWrite, PartialRead

--- cpp/Platform.Numbers/Bit.h
+++ cpp/Platform.Numbers/Bit.h
@@ -1,45 +1,60 @@
-﻿namespace Platform::Numbers
+﻿namespace Platform::Numbers::Bit
 {
-    class Bit
+    constexpr std::int64_t Count(std::int64_t x)
     {
-        public: static std::int64_t Count(std::int64_t x)
+        std::int64_t n = 0;
+        while (x != 0)
         {
-            std::int64_t n = 0;
-            while (x != 0)
-            {
-                n++;
-                x &= x - 1;
-            }
-            return n;
+            n++;
+            x &= x - 1;
         }
+        return n;
+    }
 
-        public: static std::int32_t GetLowestPosition(std::uint64_t value)
+    constexpr std::int32_t GetLowestPosition(std::uint64_t value)
+    {
+        if (value == 0)
         {
-            if (value == 0)
-            {
-                return -1;
-            }
-            auto position = 0;
-            while ((value & 1UL) == 0)
-            {
-                value >>= 1;
-                ++position;
-            }
-            return position;
+            return -1;
         }
+        auto position = 0;
+        while ((value & 1UL) == 0)
+        {
+            value >>= 1;
+            ++position;
+        }
+        return position;
+    }
 
-        public: template <typename T> static T Not(T x) { return Bit<T>.Not(x); }
-
-        public: template <typename T> static T Or(T x, T y) { return Bit<T>.Or(x, y); }
-
-        public: template <typename T> static T And(T x, T y) { return Bit<T>.And(x, y); }
-
-        public: template <typename T> static T ShiftLeft(T x, std::int32_t y) { return Bit<T>.ShiftLeft(x, y); }
-
-        public: template <typename T> static T ShiftRight(T x, std::int32_t y) { return Bit<T>.ShiftRight(x, y); }
-
-        public: template <typename T> static T PartialWrite(T target, T source, std::int32_t shift, std::int32_t limit) { return Bit<T>.PartialWrite(target, source, shift, limit); }
-
-        public: template <typename T> static T PartialRead(T target, std::int32_t shift, std::int32_t limit) { return Bit<T>.PartialRead(target, shift, limit); }
-    };
+    template <typename T>
+    constexpr T PartialWrite(T target, T source, int shift, int limit)
+    {
+        if (shift < 0)
+        {
+            shift = sizeof(T) + shift;
+        }
+        if (limit < 0)
+        {
+            limit = sizeof(T) + limit;
+        }
+        auto sourceMask = ~(std::numeric_limits<T>::max() << limit) & std::numeric_limits<T>::max();
+        auto targetMask = ~(sourceMask << shift);
+        return target & targetMask | (source & sourceMask) << shift;
+    }
+    
+    template <typename T>
+    constexpr T PartialRead(T target, T shift, int limit)
+    {
+        if (shift < 0)
+        {
+            shift = sizeof(T) + shift;
+        }
+        if (limit < 0)
+        {
+            limit = sizeof(T) + limit;
+        }
+        var sourceMask = ~(std::numeric_limits<T>::max() << limit) & std::numeric_limits<T>::max();
+        var targetMask = sourceMask << shift;
+        return (target & targetMask) >> shift;
+    }
 }
