88f8530787e99d564278212d1f810c85a5b0e76c
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-28 22:59:50 +0600

    Add benchmarks

--- csharp/Platform.Numbers.Benchmarks/ArithmeticBenchmarks.cs
+++ csharp/Platform.Numbers.Benchmarks/ArithmeticBenchmarks.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.Numerics;
 using BenchmarkDotNet.Attributes;
 
 #pragma warning disable CA1822 // Mark members as static
@@ -8,6 +10,14 @@ namespace Platform.Numbers.Benchmarks
     [MemoryDiagnoser]
     public class ArithmeticBenchmarks
     {
+        [Params(100, 1000)]
+        public ulong N { get; set; }
+        static T Add<T>(T left, T right)
+            where T : INumber<T>
+        {
+            return left + right;
+        }
+        
         [Benchmark]
         public int IncrementWithoutRef()
         {
@@ -38,6 +48,28 @@ namespace Platform.Numbers.Benchmarks
             var x = 1;
             Arithmetic.Decrement(ref x);
             return x;
+        }
+
+        [Benchmark]
+        public ulong AddWithGenericMath()
+        {
+            ulong result = default;
+            for (ulong i = 0; i < N; i++)
+            {
+                result = Add(i, i);
+            }
+            return result;
+        }
+        
+        [Benchmark]
+        public ulong AddByPlusOperator()
+        {
+            ulong result = default;
+            for (ulong i = 0; i < N; i++)
+            {
+                result = i + i;
+            }
+            return result;
         }
     }
 }
